dded2e1f98b63e18b496d7737a39b39ce7bdcaa8
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-01 22:18:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,13 +9,7 @@ class Anagram
     result = [] #need to iterated over anagrams to see if any of the elements are composed of the same letters as word
     new_array = array.collect {|item| # iterating over anagrams and splitting each word into an array of characters.
       item.split("").sort}
-    new_array. each do |item|
-      if item == @anagram.split("").sort
-        result << @anagram
-      else
-        result
-      end
-    end
+    new_array.select { |item| item == @anagram.split("").sort} 
 
   end
 end
