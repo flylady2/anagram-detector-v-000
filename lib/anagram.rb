4ef62132132faa8c87ebe5a24827a74f8c663134
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-01 21:28:11 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    result = [] #need to iterated over anagrams to see if any of the elements are composed of the same letters as word
+    new_anagrams = (%w(anagrams)).collect {|item| # iterating over anagrams and splitting each word into an array of characters.
+      item.split("").sort}
+    new_anagrams. each do |item|
+      if item == word.split("").sort
+        result << word
+      else
+        result
+      end
+    end
+
+  end
+end
