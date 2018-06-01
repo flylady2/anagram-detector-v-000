a84fae2142835f03dc93ae7881c2c634af9152c5
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-01 22:59:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,9 +7,12 @@ class Anagram
 
   def match(array)
     result = [] #need to iterated over anagrams to see if any of the elements are composed of the same letters as word
-    new_array = array.collect {|item| # iterating over anagrams and splitting each word into an array of characters.
-      item.split("").sort.join}
-    new_array.select { |item| item == @anagram.split("").sort.join} 
+    if array.each {|item| # iterating over anagrams and splitting each word into an array of characters.
+      item.split("").sort == @anagram.split("").sort}
+      result << item
+      result
+    end
+
 
   end
 end
